96b9a4f5f51438b7a7e5d00b804b33f8c7a51dc1
Arthur Lim <arthur.lim@gatech.edu>
2016-11-11 15:52:56 -0500

    clean up

--- memeBot/memeBot.py
+++ memeBot/memeBot.py
@@ -5,7 +5,7 @@ botId = "09627250596d98c0f84afe710d"
 
 data = {
   "bot_id"  : botId,
-  "text"    : "Posting in Python is working"
+  "text"    : "TEST"
 }
 
 req = requests.post('https://api.groupme.com/v3/bots/post', data)
